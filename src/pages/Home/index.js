002cebf188ad55ee39fa993636641b67db0db273
soyvieira <soyvieira8@gmail.com>
2023-02-18 14:42:35 -0300

    Movie list on home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react";
+import api from '../../services/api';
+import { Link } from "react-router-dom";
+import './home.css';
+
 function Home(){
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect( () => {
+
+    async function loadMovies(){
+      const response = await api.get('movie/now_playing', {
+        params: {
+          api_key: "", //Insert api key here. See documentation on https://www.themoviedb.org/ , config section.
+          //language: 'pt-BR', uncomment it to show the content in Portuguese
+          page: 1,
+        }
+      })
+      setMovies(response.data.results.slice(0,10));
+    }
+    loadMovies();
+  }, [] )  
+
   return(
-    <div>
-      <h1>Welcome Home!</h1>
+    <div className="container">
+      <div className="movie-list">
+        {movies.map( (movies) => {
+          return(
+            <article key={movies.id}>
+              <strong>{movies.title}</strong>
+              <img src={`https://image.tmdb.org/t/p/original/${movies.poster_path}`} alt={movies.title}  /> 
+              { /* https://image.tmdb.org/t/p/original/ is a static URL from API, for images */ }
+              <Link to={`/movie/${movies.id}`}>See more</Link>
+            </article>
+          )
+        }
+        )}
+      </div>
+      
     </div>
   )
 }
